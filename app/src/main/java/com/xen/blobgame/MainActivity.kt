6d6c05306ea26f6b44b0312edab135b0ff3f39ba
XenDQ <xeanxyrone@gmail.com>
2026-03-05 19:23:46 +0800

    feat: added oncreate functionality

--- app/src/main/java/com/xen/blobgame/MainActivity.kt
+++ app/src/main/java/com/xen/blobgame/MainActivity.kt
@@ -6,15 +6,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.xen.blobgame.data.remote.GameRoomApi
 import com.xen.blobgame.data.remote.JoinRequest
+import com.xen.blobgame.data.remote.NewPlayer
 import com.xen.blobgame.data.remote.PlayerApi
+import com.xen.blobgame.data.remote.RoomRequest
 import com.xen.blobgame.data.repository.GameRoomRepository
 import com.xen.blobgame.ui.composable.GameScreen
 import com.xen.blobgame.ui.composable.MainMenuScreen
 import com.xen.blobgame.ui.theme.BlobGameTheme
 import com.xen.blobgame.ui.viewmodel.GameRoomViewModel
 import com.xen.blobgame.ui.viewmodel.GameSessionViewModel
+import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -24,11 +28,12 @@ class MainActivity : ComponentActivity() {
     private lateinit var gameRoomViewModel: GameRoomViewModel
     private var currentScreen = mutableStateOf("menu")
     private var currentPlayerId: Int? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://127.0.0.1:8080")
+            .baseUrl("http://10.0.2.2:8080")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
@@ -46,18 +51,40 @@ class MainActivity : ComponentActivity() {
                 when (currentScreen.value) {
                     "menu" -> MainMenuScreen(
                         onJoinRoom = { roomId ->
-                            val id = currentPlayerId ?: 123
-                            gameRoomViewModel.joinRoom(roomId, JoinRequest(playerId = id))
+                            lifecycleScope.launch {
 
-                            currentScreen.value = "game"
-                            gameViewModel.startSession(playerId = id, roomId = roomId)
+                                val id = currentPlayerId ?: createNewPlayer(playerApi)
+                                currentPlayerId = id
+
+                                gameRoomViewModel.joinRoom(roomId, JoinRequest(playerId = id))
+
+                                currentScreen.value = "game"
+                                gameViewModel.startSession(playerId = id, roomId = roomId)
+                                gameViewModel.connect()
+                            }
                         },
-                        onCreateRoom = { /* Implement create room logic */ },
-                        onUpdateRoom = { /* Implement update room logic */ }
+                        onCreateRoom = { roomName ->
+                            lifecycleScope.launch {
+
+                                val id = currentPlayerId ?: createNewPlayer(playerApi)
+                                currentPlayerId = id
+
+                                val maxPlayers = roomName.toIntOrNull() ?: 4
+                                gameRoomViewModel.createRoom(RoomRequest(maxPlayers = maxPlayers))
+                            }
+                        },
+                        onUpdateRoom = { roomName ->
+
+                        }
                     )
                     "game" -> GameScreen(gameViewModel)
                 }
             }
         }
+    }
+
+    private suspend fun createNewPlayer(playerApi: PlayerApi): Int {
+        val player = playerApi.createPlayer(NewPlayer(name = "Player_${System.currentTimeMillis()}"))
+        return player.id
     }
 }
