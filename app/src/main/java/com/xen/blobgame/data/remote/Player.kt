64f9866b1283e0f32369044097e6ecbc06a1b564
XenDQ <xeanxyrone@gmail.com>
2026-03-09 01:35:01 +0800

    feat: added api call

--- app/src/main/java/com/xen/blobgame/data/remote/Player.kt
+++ app/src/main/java/com/xen/blobgame/data/remote/Player.kt
@@ -1,5 +1,7 @@
 package com.xen.blobgame.data.remote
 
+import retrofit2.http.POST
+import retrofit2.http.Query
 import java.util.UUID
 
 data class PlayerModel (
@@ -10,6 +12,10 @@ data class PlayerModel (
     val playerGameState: PlayerGameStateModel
 )
 
+const val playerEndpoint = "/player"
 
-class PlayerApi {
+interface PlayerApi {
+    @POST("$playerEndpoint/create")
+    suspend fun createPlayer(@Query("name") name: String): PlayerModel
+    //TODO: GAMEPLAY API INTEGRATION AND REPOS
 }
